b6a96c321fecc736b14f6478cda7477e7c361045
abdujalilc <abdujalilc@outlook.com>
2024-01-06 18:40:02 +0500

    minor changes

--- not-original/authMiddleware.js
+++ not-original/authMiddleware.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
 
-const APP_SECRET = "myappsecret",
-  USERNAME = "admin",
-  PASSWORD = "secret";
+const APP_SECRET = "myappsecret";
+const USERNAME = "admin";
+const PASSWORD = "secret";
 
 const anonOps = [
   { method: "GET", urls: ["/api/products", "/api/categories"] },
@@ -10,31 +10,32 @@ const anonOps = [
 ];
 
 module.exports = function (req, res, next) {
-  if (
-    anonOps.find(
-      (op) =>
-        op.method === req.method &&
-        op.urls.find((url) => req.url.startsWith(url))
-    )
+  if (anonOps.find((op) =>
+    op.method === req.method &&
+    op.urls.find((url) => req.url.startsWith(url)))
   ) {
     next();
-  } else if (req.url === "/login" && req.method === "POST") {
+  }
+  else if (req.url === "/login" && req.method === "POST") {
     if (req.body.username === USERNAME && req.body.password === PASSWORD) {
       res.json({
         success: true,
         token: jwt.sign({ data: USERNAME, expiresIn: "1h" }, APP_SECRET),
       });
-    } else {
+    }
+    else {
       res.json({ success: false });
     }
     res.end();
-  } else {
+  }
+  else {
     let token = req.headers["authorization"];
     if (token != null && token.startsWith("Bearer<")) {
       token = token.substring(7, token.length - 1);
       jwt.verify(token, APP_SECRET);
       next();
-    } else {
+    }
+    else {
       res.statusCode = 401;
       res.end();
     }
